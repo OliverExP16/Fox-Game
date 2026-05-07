14d000659b78fa86911d94b04f210a13e845b733
Oliver ExP <Oliver ExP@OliverExP.OliverExP>
2026-05-07 19:01:29 +0200

    Godot Projekt in Github angelegt 7.5.2026

---
+++ Script/player.gd
@@ -0,0 +1,70 @@
+extends CharacterBody2D
+
+@onready var animated_sprite = $AnimatedPlayer;  
+
+const SPEED = 170.0 
+const JUMP_VELOCITY = -380.0
+
+var took_damage = false  
+
+var Current_timer = 0 
+var Limit_timer = 30 
+var Limit_end = 120
+
+@export var spawn_position: Vector2
+
+func respawn():
+	global_position = spawn_position
+	velocity = Vector2.ZERO
+	took_damage = false
+	
+func _physics_process(delta: float) -> void: 
+	var active = (
+		Input.is_action_just_pressed("Left")
+		or 
+		Input.is_action_just_pressed("Right")
+		or 
+		Input.is_action_just_pressed("Jump")
+	)
+	
+	if active: 
+		Current_timer = 0 
+	else: 
+		Current_timer += delta 
+		
+	# Gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+				
+	# Jump
+	if Input.is_action_just_pressed("Jump") and is_on_floor(): 
+		velocity.y = JUMP_VELOCITY 
+	
+	var direction := Input.get_axis("Left", "Right")
+	if direction:
+		velocity.x = direction * SPEED
+		animated_sprite.flip_h = direction < 0; 
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	# Animationen
+	if not is_on_floor():
+		if velocity.y < -10:
+			animated_sprite.play("Jump")
+		elif velocity.y > 10:
+			animated_sprite.play("Fall")
+		else:
+			animated_sprite.play("Jump") 
+	else:
+		if direction:
+			animated_sprite.play("Run")
+			animated_sprite.flip_h = direction < 0
+		else:
+			if Current_timer >= Limit_end: 
+				animated_sprite.play("Idle_80")
+			elif Current_timer >= Limit_timer: 
+				animated_sprite.play("Idle_30")
+			else: 
+				animated_sprite.play("Idle")
+			
+	move_and_slide()
