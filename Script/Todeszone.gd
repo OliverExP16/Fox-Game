b4f14ee2c517e5a50727dabba71348aede961187
Oliver ExP <Oliver ExP@OliverExP.OliverExP>
2026-05-17 03:18:50 +0200

    Music und Mehrer andere Themen für das leveldisgn

--- Script/Todeszone.gd
+++ Script/Todeszone.gd
@@ -5,8 +5,3 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"): 
 		body.respawn()
 		
-
-
-func _on_death_area_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"): 
-		body.respawn()
--- Script/player.gd
+++ Script/player.gd
@@ -2,14 +2,18 @@ extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedPlayer;  
 
+const GRAVITY: float = 1000.0 
+
 const SPEED = 170.0 
 const JUMP_VELOCITY = -380.0
 const FLOW_VELOCITY = 50
-
 var took_damage = false  
 
-const COYOTE_TIME: float = 0.15 
+const COYOTE_TIME: float = 0.10 
 var coyote_timer: float = 0.0
+
+const JUMP_BUFFER_TIMER: float = 0.15 
+var jump_buffer_timer: float = 0.0
 
 var Current_timer = 0 
 var Limit_timer = 30 
@@ -37,30 +41,39 @@ func _physics_process(delta: float) -> void:
 		
 	# Gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-		
+		velocity.y += GRAVITY * delta
+	
 	# Jump
 	if is_on_floor(): 
 		coyote_timer = COYOTE_TIME
 	else: 
 		coyote_timer -= delta 
 	
-	if Input.is_action_just_pressed("Jump") and coyote_timer > 0: 
+	if Input.is_action_just_pressed("Jump"): 
+		jump_buffer_timer = JUMP_BUFFER_TIMER
+	else: 
+		jump_buffer_timer -= delta 
+		
+	if jump_buffer_timer > 0 and coyote_timer > 0: 
 		velocity.y = JUMP_VELOCITY
-		coyote_timer = 0
-	elif velocity.y < 0.0: 
-		if Input.is_action_just_released("Jump"): 
-			velocity.y *= 0.5 
+		jump_buffer_timer = 0 
+		coyote_timer = 0 
+		
+	if Input.is_action_just_released("Jump") and velocity.y < 0: 
+		velocity.y *= 0.5 
 		
 	# Move Left and Right 
 	var direction := Input.get_axis("Left", "Right")
 	
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(
+			velocity.x,
+			direction * SPEED,
+			900 * delta)	
 		animated_sprite.flip_h = direction < 0; 
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
+		
 	# Animationen
 	if not is_on_floor():
 		if velocity.y < -10:
@@ -80,5 +93,5 @@ func _physics_process(delta: float) -> void:
 				animated_sprite.play("Idle_30")
 			else: 
 				animated_sprite.play("Idle")
-			
+		
 	move_and_slide()
